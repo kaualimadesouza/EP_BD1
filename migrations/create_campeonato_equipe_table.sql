5ee35e897475c7cbe47bafd516d5f67d2acf5af9
Kaua <kaualimadesouza15@gmail.com>
2025-09-28 00:52:24 -0300

    updating scripts

---
+++ migrations/create_campeonato_equipe_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE campeonato_equipe (
+    id_campeonato INTEGER not null,
+    id_equipe INTEGER not null,
+    colocacao_ou_fase VARCHAR(100)
+
+    CONSTRAINT fk_campeonato FOREIGN KEY (id_campeonato) REFERENCES campeonato(id),
+    CONSTRAINT fk_equipe FOREIGN KEY (id_equipe) REFERENCES equipe(id)
+);
